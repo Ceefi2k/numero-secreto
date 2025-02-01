962cbd7315c5ed3c4444efaac367017d635b44c7
Ceefi (Fernando) <ceefiedits@gmail.com>
2025-01-31 22:04:46 -0300

    Revertendo pro original 10 números

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 let listaDeNumerosSorteados = [];
-<<<<<<< HEAD
-let numeroLimite = 50;
-=======
 let numeroLimite = 10;
->>>>>>> parent of 3a02523 (Alterado Limite para 100)
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
